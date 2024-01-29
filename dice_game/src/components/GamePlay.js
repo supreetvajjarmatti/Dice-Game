adeeafffd38707bd9c6f012c23e7704357593bcd
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-01-29 23:42:15 +0530

    Implement select number functionality with onclick event

--- dice_game/src/components/GamePlay.js
+++ dice_game/src/components/GamePlay.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import TotalScore from './TotalScore';
 import NumberSelector from './NumberSelector';
+import styled from 'styled-components';
 
 const GamePlay = () => {
   return (
-    <main>
-      <TotalScore></TotalScore>
-      <NumberSelector></NumberSelector>
-    </main>
+    <MainContainer>
+      <div className='top_section'>
+        <TotalScore></TotalScore>
+      <NumberSelector></NumberSelector></div>
+    </MainContainer>
   )
 }
 
 export default GamePlay;
+
+const MainContainer = styled.main `
+padding-top: 17px;
+
+  .top_section{
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+
+  }
+
+`
--- dice_game/src/components/NumberSelector.js
+++ dice_game/src/components/NumberSelector.js
@@ -1,26 +1,58 @@
-import React from 'react'
-import styled from 'styled-components'
+import {useState } from "react";
+import styled from "styled-components";
+const NumberSelector = () => {
+  const arrNumber = [1, 2, 3, 4, 5, 6];
 
-const arrNumber = [1,2,3,4,5,6];
+  // select number
+  const [selectedNumber, setSelectedNumber] = useState();
 
-function NumberSelector() {
-  return (
-    <div>
-        { arrNumber.map((value,i) =>(
-            <Box key={i}>{value}</Box>
-        ) )}
-    </div>
-  )
-}
+ 
 
-export default NumberSelector
+ 
+    return (
+      <NumberSelectorContainer>
+        <div className="flex">
+        {arrNumber.map((value, i) => (
+          <Box
+          isSelected={value === selectedNumber}  
+          key={i}
+            onClick={() => setSelectedNumber(value)}>{value}
+          </Box>
+        ))}
+        </div>
+        <p>Select Number</p>
+      </NumberSelectorContainer>
+    );
+  
+};
+
+export default NumberSelector;
+
+const NumberSelectorContainer = styled.div`
+
+display: flex;
+flex-direction: column;
+align-items: end;
+    .flex{
+        display: flex;
+        gap: 24px;
+    }
+
+    p{
+        font-size: 24px;
+        font-weight: 700px;
+    }
+`
 
 const Box = styled.div`
-    height: 72px;
-    width: 72px;
-    border: 1px solid black;
-    display: grid;
-    place-items: center;
-    font-size: 24px;
-    font-weight: 700;
-`
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  display: grid;
+  place-items: center;
+  font-size: 24px;
+  font-weight: 700;
+  cursor: pointer;
+  background-color: ${(props) => (props.isSelected ? "black" : "white")};
+  color: ${(props) => (!props.isSelected ? "black" : "white")};
+`;
