a8672adc22c1e74345aece1e28a18f99d7668cd6
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-01-31 23:29:47 +0530

    changes

--- dice_game/src/components/GamePlay.js
+++ dice_game/src/components/GamePlay.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TotalScore from './TotalScore';
 import NumberSelector from './NumberSelector';
 import styled from 'styled-components';
+import RoleDice from './RoleDice';
 
 const GamePlay = () => {
   return (
@@ -9,6 +10,7 @@ const GamePlay = () => {
       <div className='top_section'>
         <TotalScore></TotalScore>
       <NumberSelector></NumberSelector></div>
+      <RoleDice/>
     </MainContainer>
   )
 }
---
+++ dice_game/src/components/RoleDice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const RoleDice = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RoleDice
+
+const DiceContainer = styled.div`
+
+`
